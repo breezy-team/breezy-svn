7752f0b66483b5b63f524fa08c50198fbefb0259
Jelmer Vernooij <jelmer@samba.org>
2008-11-12 13:13:47 +0100

    Fix tests.

--- tests/mapping_implementations/test_repository.py
+++ tests/mapping_implementations/test_repository.py
@@ -1005,7 +1005,7 @@ class TestSubversionMappingRepositoryWorks(SubversionTestCase):
         self.assertEquals([
             ('file', mapping.generate_file_id(b.repository.uuid, 1, "", u"foo"), set([revid])),
             ('inventory', None, [revid]),
-            ('signatures', None, set([])),
+            ('signatures', None, set([revid])),
             ('revisions', None, [revid])], ch)
 
     def test_fetch_file_from_non_branch(self):
