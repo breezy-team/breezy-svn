fde43518841310cf03b4e14ddecdbeaa5c9d26e3
Jelmer Vernooij <jelmer@samba.org>
2009-02-17 20:20:28 +0100

    Start on 0.5.2.

--- __init__.py
+++ __init__.py
@@ -45,7 +45,7 @@ from bzrlib.transport import register_lazy_transport, register_transport_proto
 # versions ending in 'exp' mean experimental mappings
 # versions ending in 'dev' mean development version
 # versions ending in 'final' mean release (well tested, etc)
-version_info = (0, 5, 1, 'final', 0)
+version_info = (0, 5, 2, 'dev', 0)
 
 if version_info[3] == 'final':
     version_string = '%d.%d.%d' % version_info[:3]
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os, sys
 setup(name='bzr-svn',
       description='Support for Subversion branches in Bazaar',
       keywords='plugin bzr svn',
-      version='0.5.1',
+      version='0.5.2',
       url='http://bazaar-vcs.org/BzrForeignBranches/Subversion',
       download_url='http://bazaar-vcs.org/BzrSvn',
       license='GPL',
