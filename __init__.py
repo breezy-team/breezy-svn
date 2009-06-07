f7644bf55a1b45bea25d7fe235e91c9524fac906
Jelmer Vernooij <jelmer@samba.org>
2009-06-08 00:11:09 +0200

    Register Subversion smart server as first server control format, before the Bazaar smart client attempts to send POST to .bzr/smart over HTTP.

--- __init__.py
+++ __init__.py
@@ -159,7 +159,12 @@ topic_registry.register_lazy('svn-branching-schemes',
 topic_registry.register_lazy('svn-layout', 
                              'bzrlib.plugins.svn.layout',
                              'help_layout', 'Subversion repository layouts')
-BzrDirFormat.register_control_format(format.SvnRemoteFormat)
+#BzrDirFormat.register_control_server_format(format.SvnRemoteFormat)
+# Register as the first control server format, since the default smart
+# server implementation tries to do a POST request against .bzr/smart and 
+# this causes some Subversion servers to reply with 401 Authentication required
+# even though they are accessible without authentication.
+BzrDirFormat._control_server_formats.insert(0, format.SvnRemoteFormat)
 BzrDirFormat.register_control_format(format.SvnWorkingTreeDirFormat)
 branch_network_format_registry.register_lazy("subversion",
         'bzrlib.plugins.svn.branch', 'SvnBranchFormat')
