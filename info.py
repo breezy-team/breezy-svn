5ad62f418a3e8ac654324780508aa85345094598
Jelmer Vernooij <jelmer@samba.org>
2009-09-01 15:07:46 +0200

    Release 0.6.5.

--- info.py
+++ info.py
@@ -22,13 +22,13 @@ subvertpy_minimum_version = (0, 6, 1)
 # versions ending in 'exp' mean experimental mappings
 # versions ending in 'dev' mean development version
 # versions ending in 'final' mean release (well tested, etc)
-bzr_plugin_version = (0, 6, 5, 'dev', 0)
+bzr_plugin_version = (0, 6, 5, 'final', 0)
 
 bzr_commands = ["svn-import", "svn-layout"]
 
 bzr_transports = ["svn://", "svn+"]
 
-bzr_compatible_versions = [(1, x, 0) for x in [15, 16, 17]]
+bzr_compatible_versions = [(1, x, 0) for x in [15, 16, 17, 18]]
 
 bzr_minimum_version = bzr_compatible_versions[0]
 
