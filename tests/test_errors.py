c70e60751d0a1a015715ee38c6fcb6e6935e94ff
Jelmer Vernooij <jelmer@samba.org>
2011-07-26 16:01:14 +0200

    Fix formatting.

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -48,43 +48,65 @@ class TestConvertError(TestCase):
     def test_decorator_known(self):
         @convert_svn_error
         def test_throws_svn():
-            raise subvertpy.SubversionException("Connection closed", subvertpy.ERR_RA_SVN_CONNECTION_CLOSED)
+            raise subvertpy.SubversionException("Connection closed",
+                subvertpy.ERR_RA_SVN_CONNECTION_CLOSED)
 
         self.assertRaises(ConnectionReset, test_throws_svn)
 
     def test_convert_error_unknown(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("foo", -4)),
-                subvertpy.SubversionException)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("foo", -4)),
+            subvertpy.SubversionException)
 
     def test_convert_dav_request_failed(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("foo", subvertpy.ERR_RA_DAV_REQUEST_FAILED)), DavRequestFailed)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("foo",
+                subvertpy.ERR_RA_DAV_REQUEST_FAILED)), DavRequestFailed)
 
     def test_convert_malformed(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("foo", subvertpy.ERR_RA_SVN_MALFORMED_DATA)), TransportError)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("foo",
+                subvertpy.ERR_RA_SVN_MALFORMED_DATA)), TransportError)
 
     def test_convert_error_reset(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Connection closed", subvertpy.ERR_RA_SVN_CONNECTION_CLOSED)), ConnectionReset)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Connection closed",
+                subvertpy.ERR_RA_SVN_CONNECTION_CLOSED)), ConnectionReset)
 
     def test_convert_error_lock(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Working copy locked", subvertpy.ERR_WC_LOCKED)), LockActive)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Working copy locked",
+                subvertpy.ERR_WC_LOCKED)), LockActive)
 
     def test_convert_perm_denied(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Permission Denied", subvertpy.ERR_RA_NOT_AUTHORIZED)), PermissionDenied)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Permission Denied",
+                subvertpy.ERR_RA_NOT_AUTHORIZED)), PermissionDenied)
 
     def test_convert_unexpected_end(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Unexpected end of stream", subvertpy.ERR_INCOMPLETE_DATA)), UnexpectedEndOfContainerError)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Unexpected end of stream",
+                subvertpy.ERR_INCOMPLETE_DATA)), UnexpectedEndOfContainerError)
 
     def test_convert_unknown_hostname(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Unknown hostname 'bla'", subvertpy.ERR_UNKNOWN_HOSTNAME)), ConnectionError)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Unknown hostname 'bla'",
+                subvertpy.ERR_UNKNOWN_HOSTNAME)), ConnectionError)
 
     def test_not_implemented(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Remote server doesn't support ...", subvertpy.ERR_RA_NOT_IMPLEMENTED)), NotImplementedError)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Remote server doesn't support ...",
+                subvertpy.ERR_RA_NOT_IMPLEMENTED)), NotImplementedError)
 
     def test_proppatch_failed(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Proppatch failed", subvertpy.ERR_RA_DAV_PROPPATCH_FAILED)), PropertyChangeFailed)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Proppatch failed",
+                subvertpy.ERR_RA_DAV_PROPPATCH_FAILED)), PropertyChangeFailed)
 
     def test_hook_failed(self):
-        self.assertIsInstance(convert_error(subvertpy.SubversionException("Hook failed", subvertpy.ERR_REPOS_HOOK_FAILURE)), TipChangeRejected)
+        self.assertIsInstance(convert_error(
+            subvertpy.SubversionException("Hook failed",
+                subvertpy.ERR_REPOS_HOOK_FAILURE)), TipChangeRejected)
 
     def test_decorator_nothrow(self):
         @convert_svn_error
